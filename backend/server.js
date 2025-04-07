7472d98eb50be4aabd5ff8ccefaf4c883a0d906d
JayaKumarNalabai <n190927@rguktn.ac.in>
2025-04-07 08:21:04 +0530

    Fix preflight in server.js

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,7 @@ app.use(express.json());
 app.options('*', cors());
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
